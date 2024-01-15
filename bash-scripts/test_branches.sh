1b1a2f6c7596cdc280844148c1c5608c96bd3ad1
Osma S. Rautila <osma.rautila@helsinki.fi>
2024-01-15 21:02:55 +0200

    hyperfine

--- bash-scripts/test_branches.sh
+++ bash-scripts/test_branches.sh
@@ -12,8 +12,12 @@ fi
 git checkout $branch1
 cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p tmp_$branch1
 
+hyperfine -w 2 "cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p tmp_${branch1}"
+
 git checkout $branch2
 cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p tmp_$branch2
+
+hyperfine -w 2 "cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p tmp_${branch2}"
 
 
 python ./python-scripts/assert_equal_hst.py tmp_${branch1}_bhst.hst.gz tmp_${branch2}_bhst.hst.gz
