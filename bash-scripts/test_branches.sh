fa698fc763e52deed406d00284d675c4a34a4d84
Osma S. Rautila <osma.rautila@helsinki.fi>
2024-01-15 20:58:39 +0200

    tmp prefix

--- bash-scripts/test_branches.sh
+++ bash-scripts/test_branches.sh
@@ -10,10 +10,10 @@ then
 fi
 
 git checkout $branch1
-cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p $branch1
+cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p tmp_$branch1
 
 git checkout $branch2
-cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p $branch2
+cargo run --release --bin haptk bhst haptk/tests/data/1k_genomes_test.vcf.gz -c chr9:27573534 -s all -p tmp_$branch2
 
 
-python ./python-scripts/assert_equal_hst.py ${branch1}_bhst.hst.gz ${branch2}_bhst.hst.gz
+python ./python-scripts/assert_equal_hst.py tmp_${branch1}_bhst.hst.gz tmp_${branch2}_bhst.hst.gz
