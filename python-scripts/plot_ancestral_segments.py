26efc9d105fcb3587534c71fc6b1a681964c511a
Osma S. Rautila <osma.rautila@helsinki.fi>
2024-12-12 19:40:54 +0200

    colors to ancestral segment lengths

--- python-scripts/plot_ancestral_segments.py
+++ python-scripts/plot_ancestral_segments.py
@@ -5,22 +5,28 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker
 import argparse
 
-def visualize(df, rec_rates, snps, min, max, gargs, args):
-    # df = df.sort_values(by=['stop', 'len'], ascending=False)
-    df = df.sort_values(by=['start', 'len'], ascending=[True,False])
+def visualize(df, rec_rates, snps, min, max, gargs, args, sample_colors):
+    # df = df.sort_values(by=['stop', 'len'], ascending=[False, False])
+    # df = df.sort_values(by=['start', 'len'], ascending=[True,False])
+    df = df.sort_values(by=['len', 'start'], ascending=[False, True])
+
+    if sample_colors:
+        df = df.sort_values(by=['group_order', 'len', 'start'], ascending=[False, False, True])
 
     y = [
         list(item) for item in zip(df['start'].to_list(), df['stop'].to_list())
     ]
     samples = df.id.values.tolist()
-    samples = [x[4:] for x in samples]
+    # samples = [x[4:] for x in samples]
 
     fig = plt.figure(figsize=(args.width, args.height))
+
     ax = fig.add_subplot(
         111,
         ylabel="sample",
         xlabel=gargs["xlabel"],
     )
+
     lines_color = '#e76f9b'
     lines_color = '#df6a95'
     # lines_color = '#8282e9'
@@ -31,8 +37,12 @@ def visualize(df, rec_rates, snps, min, max, gargs, args):
 
     plt.setp(ax.get_xticklabels(), rotation=0, fontsize='small')
     ids = df.id.values.tolist()
+
     for idx in range(0, len(samples)):
-        ax.plot(y[idx], [ids[idx], ids[idx]], c=lines_color, linewidth='4')
+        if sample_colors:
+            ax.plot(y[idx], [ids[idx], ids[idx]], c=sample_colors[samples[idx]], linewidth='5.9')
+        else:
+            ax.plot(y[idx], [ids[idx], ids[idx]], c=lines_color, linewidth='5')
 
     if rec_rates:
         ax.plot(rec_rates['pos'], rec_rates['rate'], c=rec_color, linewidth='1')
@@ -69,20 +79,22 @@ def visualize(df, rec_rates, snps, min, max, gargs, args):
 def load_df(name):
     df = pd.read_csv(name)
 
-    sorted_start = df['start'].sort_values().tolist()
-    sorted_stop = df['stop'].sort_values().tolist()
+    # Cut x-axis at % of samples if so wanted
+    percentage = 10
 
-    # cutoff = int(len(sorted_start) * 0.2)
-    cutoff = int(len(sorted_start) * 0.10)
+    if percentage and percentage != 0:
+        sorted_start = df['start'].sort_values().tolist()
+        sorted_stop = df['stop'].sort_values().tolist()
+        cutoff = int(len(sorted_start) * (percentage / 100))
+        start_last = sorted_start[cutoff]
+        stop_last = sorted_stop[-cutoff]
+        df['start'] = df['start'].apply(lambda x: start_last if x < start_last else x)
+        df['stop'] = df['stop'].apply(lambda x: stop_last if x > stop_last else x)
 
-    start_last = sorted_start[cutoff]
-    stop_last = sorted_stop[-cutoff]
 
-    df['start'] = df['start'].apply(lambda x: start_last if x < start_last else x)
-    df['stop'] = df['stop'].apply(lambda x: stop_last if x > stop_last else x)
-
-
+    # Add length column
     df['len'] = df['stop'] - df['start']
+
     return df
 
 def load_rec_rates(df, name, min, max):
@@ -108,6 +120,7 @@ pd.set_option('display.max_rows', 1000)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('data', type=str)
+parser.add_argument('--ids', nargs="+", type=str)
 parser.add_argument('-r', '--rec-rates', type=str)
 parser.add_argument('-c', '--coords', type=str, required=True)
 parser.add_argument('--gene', type=str)
@@ -140,5 +153,44 @@ graph_args = {
         "xlabel": chr,
         }
 
-visualize(df, rec_rates, snps, min, max, graph_args, args)
+
+# Create list of samples to tag
+samples_to_tag = []
+if args.ids:
+    for file in args.ids:
+        ids = []
+        file = open(file, 'r')
+        for line in file.readlines():
+            ids.append(line.strip())
+
+        samples_to_tag.append(ids)
+
+# kyme, kanta-hame, karjala, paijat-hame, pirkanmaa
+colors=["#866adb", "#f7708b", "#6ec9b8", "#ffe1bd", "#6ed4ff"]
+# colors=["#c34757", "#9a69b1", "#4071ab", "#90a720", "#d75bae"]
+
+sample_colors = {}
+for (vec, color) in zip(samples_to_tag, colors):
+    for sample in vec:
+        sample_colors[sample] = color
+
+if sample_colors:
+    df['color'] = df.apply(lambda row: sample_colors[row.id], axis = 1)
+
+    tuples = []
+    for color in df.color.unique():
+        color_df = df.loc[df['color'] == color]
+        sum = color_df.len.sum()
+        tuples.append((color, sum/color_df.shape[0]))
+
+    tuples = sorted(tuples, key=lambda x: float(x[1]))
+
+    order = {}
+    for i, elem in enumerate(tuples):
+        order[elem[0]] = i
+
+    # Lowest order number has lowest haplotype sharing on avg
+    df['group_order'] = df.apply(lambda row: order[row.color], axis = 1)
+
+visualize(df, rec_rates, snps, min, max, graph_args, args, sample_colors)
 
