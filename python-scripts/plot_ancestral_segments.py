69ba17e6b31e0d749af016c3a405a5a28f67c019
Osma S. Rautila <osma.rautila@helsinki.fi>
2025-09-05 18:37:54 +0300

    feat(better anc segment sharing visualization) add core haplotype and position bed file support

--- python-scripts/plot_ancestral_segments.py
+++ python-scripts/plot_ancestral_segments.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker
 import argparse
 
-def visualize(df, rec_rates, snps, min, max, gargs, args, sample_colors):
+def visualize(df, rec_rates, snps, min, max, gargs, args, sample_colors, hap_start, hap_stop):
     # df = df.sort_values(by=['stop', 'len'], ascending=[False, False])
     # df = df.sort_values(by=['start', 'len'], ascending=[True,False])
     df = df.sort_values(by=['len', 'start'], ascending=[False, True])
@@ -13,11 +13,8 @@ def visualize(df, rec_rates, snps, min, max, gargs, args, sample_colors):
     if sample_colors:
         df = df.sort_values(by=['group_order', 'len', 'start'], ascending=[False, False, True])
 
-    y = [
-        list(item) for item in zip(df['start'].to_list(), df['stop'].to_list())
-    ]
+    y = [list(item) for item in zip(df['start'].to_list(), df['stop'].to_list())]
     samples = df.id.values.tolist()
-    # samples = [x[4:] for x in samples]
 
     fig = plt.figure(figsize=(args.width, args.height))
 
@@ -27,54 +24,89 @@ def visualize(df, rec_rates, snps, min, max, gargs, args, sample_colors):
         xlabel=gargs["xlabel"],
     )
 
-    lines_color = '#e76f9b'
+    hap_color = "#D0BDF0"
     lines_color = '#df6a95'
-    # lines_color = '#8282e9'
-    # lines_color = '#000'
     snp_color = '#FFF'
     text_color = '#000'
     rec_color = '#000'
 
-    plt.setp(ax.get_xticklabels(), rotation=0, fontsize='small')
     ids = df.id.values.tolist()
 
     for idx in range(0, len(samples)):
         if sample_colors:
-            ax.plot(y[idx], [ids[idx], ids[idx]], c=sample_colors[samples[idx]], linewidth='5.9')
+            lines_color = sample_colors[samples[idx]]
+
+        x_start = y[idx][0]
+        x_stop = y[idx][1]
+
+        if hap_start is not None and hap_stop is not None:
+            ax.plot([x_start, hap_start], [ids[idx], ids[idx]], c=lines_color, linewidth='5')
+            ax.plot([hap_stop, x_stop], [ids[idx], ids[idx]], c=lines_color, linewidth='5')
+            ax.plot([hap_start, hap_stop], [ids[idx], ids[idx]], c=hap_color, linewidth='5')
         else:
-            ax.plot(y[idx], [ids[idx], ids[idx]], c=lines_color, linewidth='5')
+            ax.plot([x_start, x_stop], [ids[idx], ids[idx]], c=lines_color, linewidth='5')
 
     if rec_rates is not None:
         ax.plot(rec_rates['pos'], rec_rates['rate'], c=rec_color, linewidth='1')
 
-    plt.yticks(fontsize=3)
-    plt.xlim(xmin=min, xmax=max)
-    ax.get_xaxis().set_major_formatter(
-        matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 
-    for item in snps:
-        ax.axvline(x=item[1] - 50,
-                   ls='-',
-                   c=snp_color,
-                   linewidth='2')
+    count = 0
+    for i, item in enumerate(snps):
 
-        plt.text(item[1] + 500, len(y)/2, item[0], rotation=0, c=text_color, fontsize='x-large')
+        padding = 110
+        size = 'x-large'
 
+        if count >= 3:
+            count = 1
+        else:
+            count += 1
+
+        if i == 0:
+            color = "#000"
+            ax.axvline(x=item[1] - 50, ls='-', c=color, linewidth='2')
+            plt.text(item[1] + padding, len(y)/2, item[0], rotation=0, c="#fff", fontsize=size, backgroundcolor = "#000")
+        else:
+            ax.axvline(x=item[1] - 50, ls='-', c=snp_color, linewidth='2')
+
+            if count == 1:
+                height = 13
+                plt.text(item[1] + padding, height, item[0], rotation=0, c=text_color, fontsize=size, backgroundcolor = "#fff")
+            elif count == 2:
+                height = 26
+                plt.text(item[1] + padding, height, item[0], rotation=0, c=text_color, fontsize=size, backgroundcolor = "#fff")
+            else:
+                # height = len(y)/12
+                height = 39
+                # print(len(y))
+                plt.text(item[1] + padding, height, item[0], rotation=0, c=text_color, fontsize=size, backgroundcolor = "#fff")
+                
+            
+
+    setup_plot(ax)
+
+    if args.output:
+        plt.savefig(args.output, dpi=600)
+    else:
+        plt.show()
+
+def setup_plot(ax):
     ax.get_yaxis().set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['left'].set_visible(False)
 
-    ax.tick_params(axis='both', which='minor', labelsize=10)
-    ax.tick_params(axis='both', which='major', labelsize=10)
+    plt.yticks(fontsize=3)
+    plt.xlim(xmin=min, xmax=max)
+    ax.get_xaxis().set_major_formatter( matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 
+    ax.tick_params(axis='y', which='minor', labelsize=10)
+    ax.tick_params(axis='y', which='major', labelsize=10)
+
+    ax.tick_params(axis='x', which='minor', labelsize=10)
+    ax.tick_params(axis='x', which='major', labelsize=15)
+    ax.xaxis.label.set_size(15)
     plt.tight_layout()
-
-    if args.output:
-        plt.savefig(args.output, dpi=600)
-    else:
-        plt.show()
 
 def load_df(name):
     df = pd.read_csv(name)
@@ -126,28 +158,50 @@ parser.add_argument('-c', '--coords', type=str, required=True)
 parser.add_argument('--gene', type=str)
 parser.add_argument('-x', '--width', type=float, default=14.40)
 parser.add_argument('-y', '--height', type=float, default=25.60)
+parser.add_argument('--hap-start', type=int, required=False)
+parser.add_argument('--hap-stop', type=int, required=False)
 parser.add_argument('-o', '--output', type=str, required=False)
+parser.add_argument('--positions', type=str, required=False)
 args = parser.parse_args()
 
 df = load_df(args.data)
 
+hap_start = None
+hap_stop = None
+rec_rates = None
+gene = None
+
 coords = args.coords
 chr = coords.split(":")[0]
 pos = coords.split(":")[1]
-gene = args.gene
 
-if gene:
-    snps = [(gene, int(pos))]
-else:
-    snps = [("", int(pos))]
+if args.gene:
+    gene = args.gene
+if args.hap_start:
+    hap_start = args.hap_start
+if args.hap_stop:
+    hap_stop = args.hap_stop
 
 min = df['start'].min() - 8000
 max = df['stop'].max() + 10000
 
+if gene is not None:
+    snps = [(gene, int(pos))]
+else:
+    snps = [("", int(pos))]
+    
+if args.positions:
+    with open(args.positions, 'r') as file:
+        for line in file:
+            line = line.strip()
+            line = line.split(',')
+            name = line[0]
+            pos = int(line[1])
+            if pos < max - 10000 and pos > min + 10000:
+                snps.append((name, pos))
+
 if args.rec_rates:
     rec_rates = load_rec_rates(df, args.rec_rates, min, max)
-else:
-    rec_rates = None
 
 graph_args = {
         "xlabel": chr,
@@ -192,5 +246,5 @@ if sample_colors:
     # Lowest order number has lowest haplotype sharing on avg
     df['group_order'] = df.apply(lambda row: order[row.color], axis = 1)
 
-visualize(df, rec_rates, snps, min, max, graph_args, args, sample_colors)
+visualize(df, rec_rates, snps, min, max, graph_args, args, sample_colors, hap_start, hap_stop)
 
